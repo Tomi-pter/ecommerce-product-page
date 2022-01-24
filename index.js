cb6516bda15420f4c2f22061a0ce2e59a18e4e92
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-01-24 23:00:26 +0100

    changes

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
-const body = document.querySelector("body");
 const dimmer = document.querySelector(".dim");
 const closeBtn = document.querySelector(".close");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const displayImg = document.querySelector(".product");
+const thumbnail = document.querySelectorAll(".thumbnail img");
+const reduceItem = document.querySelector(".reduceI");
+const increaseItem = document.querySelector(".increaseI");
+const itemAmnt = document.querySelector(".item-amount p");
+const addCart = document.querySelector(".add-cart");
+const cartAmnt = document.querySelector(".headCart div");
+const totalAmt = document.querySelector(".totAmt");
+const total = document.querySelector(".total");
+const cartMain = document.querySelector(".cartMain");
+const cartDiv = document.querySelector(".cartMain>div");
+const checkoutBtn = document.querySelector(".cartMain>button");
+const cart = document.querySelector(".headCart");
 
 menu.addEventListener("click", () => {
     nav.classList.add("clicked");
@@ -47,3 +58,41 @@ function nextImg() {
         displayImg.id = num;
     }
 }
+
+const thumbArr = Array.from(thumbnail);
+
+thumbArr.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        displayImg.src = `images/image-product-${index + 1}.jpg`;
+    });
+});
+
+reduceItem.addEventListener("click", () => {
+    let amnt = +itemAmnt.textContent;
+    amnt > 0 && (amnt -= 1);
+    itemAmnt.textContent = amnt;
+});
+
+increaseItem.addEventListener("click", () => {
+    let amnt = +itemAmnt.textContent;
+    amnt += 1;
+    itemAmnt.textContent = amnt;
+});
+
+addCart.addEventListener("click", () => {
+    let cAmount = +cartAmnt.textContent; +
+    itemAmnt.textContent > 0 &&
+        ((cartAmnt.style.display = "block"),
+            (cAmount += +itemAmnt.textContent),
+            (cartAmnt.textContent = cAmount),
+            (totalAmt.innerHTML = `${cAmount} <span class='total'>$${(
+      cAmount * 125
+    ).toFixed(2)}</span>`),
+            (itemAmnt.textContent = 0));
+});
+
+cart.addEventListener("click", () => {
+    cartMain.classList.contains("clicked") ?
+        cartMain.classList.remove("clicked") :
+        cartMain.classList.add("clicked");
+});
